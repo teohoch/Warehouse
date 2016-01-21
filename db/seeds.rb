5cab6781c13f8e37f03c7fef968a928ac2e1f44d
Teodoro Hochfärber Salinas <tehoch2@gmail.com>
2016-01-21 03:41:13 -0300

    Added some things to the seed file, fixed some submit buttons

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,25 @@ fuser = User.new(:email => "teohoch2@gmail.com",
                  :password_confirmation => 'password')
 fuser.save
 fuser.add_role :admin
+
+bodega = Bodega.new(:name => "Principal",
+                    :location => "Primer Piso",
+                    :description => "Bodega Principal para todo")
+bodega.save
+
+proveedor = Provider.new( :name => "Pfizer",
+                          :address=> "Santiago, Providencia",
+                          :phone => "234234234",
+                          :rut => "17995652-7")
+proveedor.save
+
+articulo_1 = Articulo.new(:name => "Gasa",
+                          :description => "Mantiene las heridas secas",
+                          :area => "General")
+articulo_1.save
+
+
+articulo_2 = Articulo.new(:name => "Povidona",
+                          :description => "Desinfectante",
+                          :area => "General")
+articulo_2.save
